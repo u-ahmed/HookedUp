8d6e282456a8eeac86bc3ee7913e5b1be0909332
Umar Ahmed <solar_flare1@hotmail.com>
2015-07-28 14:44:03 +0100

    Delete youtubeclick.js

--- youtubeclick.js
+++
@@ -1,9 +0,0 @@
-<script>
-function youtubeclick {
-var y = //youtube username;
-window.location.href = 'www.youtube.com/user/'+ y + '/videos';
-var x = document.getElementsByClassName("yt-uix-sessionlink yt-uix-tile-link  spf-link  yt-ui-ellipsis yt-ui-ellipsis-2").href;
-var videoid = x.substring(7);
-window.history.go(-1);
-}
-</script>
