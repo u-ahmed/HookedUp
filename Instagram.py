890422f8542f8b6678a7bcd62362e89f34e7cd5b
Umar Ahmed <solar_flare1@hotmail.com>
2015-07-29 23:08:08 +0100

    Should work, no idea why it doesn't.

--- Instagram.py
+++ Instagram.py
@@ -1,6 +1,8 @@
 from instagram.client import InstagramAPI
 
-access_token = ""
-client_secret = ""
+access_token = "233291163.8a612cd.c49f77de073746e9a2f53116b720302e"
+client_secret = "f17ce3c97eb14fd2b03f123c1900f973"
 api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-recent_media, next = api.user_recent_media(user_id)
+recent_media, next_ = api.user_recent_media(user_id="userid", count=10)
+for media in recent_media:
+   print (media.caption.text)
