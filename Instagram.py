92dd4f9cb74a33fba9a990c808e11f92fa90b75a
Umar Ahmed <solar_flare1@hotmail.com>
2015-07-30 13:03:06 +0100

    insta

--- Instagram.py
+++ Instagram.py
@@ -1,8 +1,17 @@
 from instagram.client import InstagramAPI
+import httplib2
+import json
+import requests
+import sys
 
+client_id = "8a612cd650344523808233c0468c80ed"
 access_token = "233291163.8a612cd.c49f77de073746e9a2f53116b720302e"
 client_secret = "f17ce3c97eb14fd2b03f123c1900f973"
+redirect_uri = "http://u-ahmed.github.io/HookedUp/instagram.html" 
+scope = ''
+user_id = "233291163"
+
 api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-recent_media = api.user_recent_media(user_id='ahmed_enigma', count=10)
+recent_media, next = api.user_recent_media(user_id='user_id', count=10)
 for media in recent_media:
-   print (media.images['standard_resolution'].url)
+   print (media.text)
